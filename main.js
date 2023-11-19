7925af85bcd8e7ea3192b9faee5ae743511fc198
Yugin Pro <yugin.pro@gmail.com>
2023-11-19 18:40:02 +0100

    del document waiting

--- main.js
+++ main.js
@@ -2,10 +2,4 @@ import { newFirebaseBeacon } from './FirebaseBeacon.js'
 import { URL } from './private/const.js'
 
 
-if (document.readyState === "loading") {
-    // Loading hasn't finished yet
-    document.addEventListener("DOMContentLoaded", () => window.fbeacon = newFirebaseBeacon(URL));
-  } else {
-    // `DOMContentLoaded` has already fired
-    window.fbeacon = newFirebaseBeacon(URL);
-  }
+window.fbeacon = newFirebaseBeacon(URL)
