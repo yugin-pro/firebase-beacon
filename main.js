a6d0b5b061ee349258a0d036a7bbb26d07ac2b5d
Yugin Pro <yugin.pro@gmail.com>
2023-11-19 17:19:30 +0100

    add fireing condition

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import { newFirebaseBeacon } from './FirebaseBeacon.js'
 import { URL } from './private/const.js'
 
-window.fbeacon = newFirebaseBeacon(URL)
+
+if (document.readyState === "loading") {
+    // Loading hasn't finished yet
+    document.addEventListener("DOMContentLoaded", () => window.fbeacon = newFirebaseBeacon(URL));
+  } else {
+    // `DOMContentLoaded` has already fired
+    window.fbeacon = newFirebaseBeacon(URL);
+  }
